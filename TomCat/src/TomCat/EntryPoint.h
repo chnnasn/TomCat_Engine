79956efa5086fa1d81887ce3f7129ad14fe40c4a
ch <127966448+chnnasn@users.noreply.github.com>
2025-09-07 01:13:29 +0800

    day2：利用spglog增加日志功能

--- TomCat/src/TomCat/EntryPoint.h
+++ TomCat/src/TomCat/EntryPoint.h
@@ -6,6 +6,11 @@ extern TomCat::Application* TomCat::CreateApplication();
 
 int main(int argc,char** argv) {
 
+	TomCat::Log::Init();
+
+	TomCat_Core_Warn("Init LOG");
+
+	TomCat_Info("Hello");
 
 	auto app = TomCat::CreateApplication();
 
---
+++ TomCat/src/TomCat/Log.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+
+#include "spdlog/spdlog.h"
+
+namespace TomCat {
+	class TomCat_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger;}
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClinetLogger;}
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClinetLogger;
+
+	};
+}
+
+
+//服务端日志宏
+#define TomCat_Core_Error(...) ;;TomCat::Log::GetCoreLogger()->error(__VA_ARGS__);
+#define TomCat_Core_Warn(...) ;;TomCat::Log::GetCoreLogger()->warn(__VA_ARGS__);
+#define TomCat_Core_Info(...) ;;TomCat::Log::GetCoreLogger()->info(__VA_ARGS__);
+#define TomCat_Core_Trace(...) ;;TomCat::Log::GetCoreLogger()->trace(__VA_ARGS__);
+#define TomCat_Core_Fatal(...) ;;TomCat::Log::GetCoreLogger()->fatal(__VA_ARGS__);
+
+//客户端日志宏
+#define TomCat_Error(...) ;;TomCat::Log::GetClientLogger()->error(__VA_ARGS__);
+#define TomCat_Warn(...) ;;TomCat::Log::GetClientLogger()->warn(__VA_ARGS__);
+#define TomCat_Info(...) ;;TomCat::Log::GetClientLogger()->info(__VA_ARGS__);
+#define TomCat_Trace(...) ;;TomCat::Log::GetClientLogger()->trace(__VA_ARGS__);
+#define TomCat_Fatal(...) ;;TomCat::Log::GetClientLogger()->fatal(__VA_ARGS__);
+
