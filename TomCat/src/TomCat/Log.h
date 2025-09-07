23cc552d7c48f2c0d590999bb028b8afc3f6ebe3
ch <127966448+chnnasn@users.noreply.github.com>
2025-09-07 12:16:46 +0800

    Day 3: Premake DLL bootstrap

--- TomCat/src/TomCat/Log.h
+++ TomCat/src/TomCat/Log.h
@@ -24,16 +24,16 @@ namespace TomCat {
 
 
 //服务端日志宏
-#define TomCat_Core_Error(...) ;;TomCat::Log::GetCoreLogger()->error(__VA_ARGS__);
-#define TomCat_Core_Warn(...) ;;TomCat::Log::GetCoreLogger()->warn(__VA_ARGS__);
-#define TomCat_Core_Info(...) ;;TomCat::Log::GetCoreLogger()->info(__VA_ARGS__);
-#define TomCat_Core_Trace(...) ;;TomCat::Log::GetCoreLogger()->trace(__VA_ARGS__);
-#define TomCat_Core_Fatal(...) ;;TomCat::Log::GetCoreLogger()->fatal(__VA_ARGS__);
+#define TC_Core_Error(...) ;;TomCat::Log::GetCoreLogger()->error(__VA_ARGS__);
+#define TC_Core_Warn(...) ;;TomCat::Log::GetCoreLogger()->warn(__VA_ARGS__);
+#define TC_Core_Info(...) ;;TomCat::Log::GetCoreLogger()->info(__VA_ARGS__);
+#define TC_Core_Trace(...) ;;TomCat::Log::GetCoreLogger()->trace(__VA_ARGS__);
+#define TC_Core_Fatal(...) ;;TomCat::Log::GetCoreLogger()->fatal(__VA_ARGS__);
 
 //客户端日志宏
-#define TomCat_Error(...) ;;TomCat::Log::GetClientLogger()->error(__VA_ARGS__);
-#define TomCat_Warn(...) ;;TomCat::Log::GetClientLogger()->warn(__VA_ARGS__);
-#define TomCat_Info(...) ;;TomCat::Log::GetClientLogger()->info(__VA_ARGS__);
-#define TomCat_Trace(...) ;;TomCat::Log::GetClientLogger()->trace(__VA_ARGS__);
-#define TomCat_Fatal(...) ;;TomCat::Log::GetClientLogger()->fatal(__VA_ARGS__);
+#define TC_Error(...) ;;TomCat::Log::GetClientLogger()->error(__VA_ARGS__);
+#define TC_Warn(...) ;;TomCat::Log::GetClientLogger()->warn(__VA_ARGS__);
+#define TC_Info(...) ;;TomCat::Log::GetClientLogger()->info(__VA_ARGS__);
+#define TC_Trace(...) ;;TomCat::Log::GetClientLogger()->trace(__VA_ARGS__);
+#define TC_Fatal(...) ;;TomCat::Log::GetClientLogger()->fatal(__VA_ARGS__);
 
