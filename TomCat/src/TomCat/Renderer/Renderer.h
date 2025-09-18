8b125a95aed9f73511aab7d76797c57df0f6b07b
ch <127966448+chnnasn@users.noreply.github.com>
2025-09-18 21:51:02 +0800

    Day26：增加转换，实现对独立物体的移动

--- TomCat/src/TomCat/Renderer/Renderer.h
+++ TomCat/src/TomCat/Renderer/Renderer.h
@@ -14,7 +14,9 @@ namespace TomCat {
 		static void BeginScene(OrthographicCamera& Camera);
 		static void EndScene();
 
-		static void Submit(const std::shared_ptr<Shader>& shader , const std::shared_ptr<VertexArray>& vertexArray);
+		static void Submit(const std::shared_ptr<Shader>& shader , 
+			const std::shared_ptr<VertexArray>& vertexArray, 
+			const glm::mat4& transform = glm::mat4(1.0f));
 
 		inline static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); };
 	
