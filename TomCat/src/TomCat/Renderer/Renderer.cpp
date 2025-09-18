d8bd8ba698ff14b4990670e40b898cfda8860d41
ch <127966448+chnnasn@users.noreply.github.com>
2025-09-18 17:26:38 +0800

    Day 23: Camera

--- TomCat/src/TomCat/Renderer/Renderer.cpp
+++ TomCat/src/TomCat/Renderer/Renderer.cpp
@@ -2,14 +2,21 @@
 #include "Renderer.h"
 
 namespace TomCat {
-	void Renderer::BeginScene()
+
+	Renderer:: SceneData* Renderer::m_SceneData = new Renderer::SceneData;
+
+	void Renderer::BeginScene(OrthographicCamera& Camera)
 	{
+		m_SceneData->VertexProjectinMatrix = Camera.GetViewProjectionMatrix();
 	}
 	void Renderer::EndScene()
 	{
 	}
-	void Renderer::Submit(const std::shared_ptr<VertexArray>& vertexArray)
+	void Renderer::Submit(const std::shared_ptr<Shader>& shader , const std::shared_ptr<VertexArray>& vertexArray)
 	{
+		shader->Bind();
+		shader->UploadUniformMat4("u_ViewProjection", m_SceneData->VertexProjectinMatrix);
+
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
 	}
