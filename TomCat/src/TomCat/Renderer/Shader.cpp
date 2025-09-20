6d6659fd5673437651c98df35e2f110bca13eb70
ch <127966448+chnnasn@users.noreply.github.com>
2025-09-20 21:29:56 +0800

    Day 31: Shader assets

--- TomCat/src/TomCat/Renderer/Shader.cpp
+++ TomCat/src/TomCat/Renderer/Shader.cpp
@@ -6,6 +6,19 @@
 
 namespace TomCat {
 
+
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None: TC_Core_Assert(false, "RendererAPI : null"); return nullptr;
+
+			case RendererAPI::API::OpenGL: return new OpenGLShader(filepath);
+		}
+		TC_Core_Assert(false, "unknown rendererapi")
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
@@ -17,6 +30,5 @@ namespace TomCat {
 		TC_Core_Assert(false, "unknown rendererapi")
 		return nullptr;
 	}
-
 
 }
