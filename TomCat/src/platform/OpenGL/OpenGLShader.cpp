0d7ed12d4f05ae0fc520a72e1969a6a7fbb4b85e
ch <127966448+chnnasn@users.noreply.github.com>
2025-09-26 01:56:00 +0800

    Day 46: Batched textures

--- TomCat/src/platform/OpenGL/OpenGLShader.cpp
+++ TomCat/src/platform/OpenGL/OpenGLShader.cpp
@@ -214,6 +214,12 @@ namespace TomCat {
 		UploadUniformInt(name, value);
 	}
 
+	void OpenGLShader::SetIntArray(const std::string& name, int* values, uint32_t count)
+	{
+		UploadUniformIntArray(name, values, count);
+	}
+
+
 	void OpenGLShader::SetFloat(const std::string& name, float value)
 	{
 		TC_PROFILE_FUNCTION();
@@ -245,6 +251,12 @@ namespace TomCat {
 	{
 		GLint Location = glGetUniformLocation(m_RendererID, name.c_str());
 		glUniform1i(Location, value);
+	}
+
+	void OpenGLShader::UploadUniformIntArray(const std::string& name, int* values, uint32_t count)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1iv(location, count, values);
 	}
 
 	void OpenGLShader::UploadUniformFloat(const std::string& name, float value)
