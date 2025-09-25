0600273cb9118fa5dc94d7d46c65dcd1a77b969b
ch <127966448+chnnasn@users.noreply.github.com>
2025-09-26 02:05:05 +0800

    Day 47: Dockspace and framebuffer

--- Z_Examples/src/Z_Examples2D.cpp
+++ Z_Examples/src/Z_Examples2D.cpp
@@ -17,12 +17,12 @@ void Z_Examples2D::OnAttach()
     TC_PROFILE_FUNCTION();
     m_CheckerboardTexture = TomCat::Texture2D::Create("assets/textures/Checkerboard.png");
 
-
-    //TomCat::FramebufferSpecification fbspec;
-    //fbspec.Width = 1920;
-    //fbspec.Height = 1080;
-    //m_Framebuffer = TomCat::Framebuffer::Create(fbspec);
+    TomCat::FramebufferSpecification fbSpec;
+    fbSpec.Width = 1280;
+    fbSpec.Height = 720;
+    m_Framebuffer = TomCat::Framebuffer::Create(fbSpec);
 }
+    
 
 void Z_Examples2D::OnDetach()
 {
@@ -40,7 +40,7 @@ void Z_Examples2D::OnUpdate(TomCat::Timestep ts)
     // Render
     {
         TC_PROFILE_SCOPE("Renderer Prep");
-        //m_Framebuffer->Bind();
+        m_Framebuffer->Bind();
 
         TomCat::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
         TomCat::RenderCommand::Clear();
@@ -56,103 +56,101 @@ void Z_Examples2D::OnUpdate(TomCat::Timestep ts)
         TomCat::Renderer2D::DrawQuad({ -0.5f, -0.5f, 0.0f }, { 1.0f, 1.0f }, m_CheckerboardTexture, 20.0f);
         TomCat::Renderer2D::EndScene();
 
-        //m_Framebuffer->Unbind();
+        m_Framebuffer->Unbind();
     }  
 }
 
 void Z_Examples2D::OnImGuiRender()
 {
     TC_PROFILE_FUNCTION();
-    //static bool DockingEnabled = true;
+    static bool DockingEnabled = true;
 
-    //if (DockingEnabled)
-    //{
-    //    static bool DockSpaceOpen = true;
-    //    static bool opt_fullscreen = true;
-    //    static bool opt_padding = false;
-    //    static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+    if (DockingEnabled)
+    {
+        static bool DockSpaceOpen = true;
+        static bool opt_fullscreen = true;
+        static bool opt_padding = false;
+        static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
 
-    //    //// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
-    //    //// because it would be confusing to have two docking targets within each others.
-    //    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-    //    if (opt_fullscreen)
-    //    {
-    //        const ImGuiViewport* viewport = ImGui::GetMainViewport();
-    //        ImGui::SetNextWindowPos(viewport->WorkPos);
-    //        ImGui::SetNextWindowSize(viewport->WorkSize);
-    //        ImGui::SetNextWindowViewport(viewport->ID);
-    //        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-    //        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-    //        window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-    //        window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-    //    }
-    //    else
-    //    {
-    //        dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
-    //    }
+        //// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+        //// because it would be confusing to have two docking targets within each others.
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+        if (opt_fullscreen)
+        {
+            const ImGuiViewport* viewport = ImGui::GetMainViewport();
+            ImGui::SetNextWindowPos(viewport->WorkPos);
+            ImGui::SetNextWindowSize(viewport->WorkSize);
+            ImGui::SetNextWindowViewport(viewport->ID);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+            window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+            window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+        }
+        else
+        {
+            dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+        }
 
-    //    // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
-    //    // and handle the pass-thru hole, so we ask Begin() to not render a background.
-    //    if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-    //        window_flags |= ImGuiWindowFlags_NoBackground;
+        // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+        // and handle the pass-thru hole, so we ask Begin() to not render a background.
+        if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+            window_flags |= ImGuiWindowFlags_NoBackground;
 
-    //    // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
-    //    // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
-    //    // all active windows docked into it will lose their parent and become undocked.
-    //    // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
-    //    // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
-    //    if (!opt_padding)
-    //        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-    //    ImGui::Begin("DockSpace Demo", &DockSpaceOpen, window_flags);
-    //    if (!opt_padding)
-    //        ImGui::PopStyleVar();
+        // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+        // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+        // all active windows docked into it will lose their parent and become undocked.
+        // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+        // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+        if (!opt_padding)
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("DockSpace Demo", &DockSpaceOpen, window_flags);
+        if (!opt_padding)
+            ImGui::PopStyleVar();
 
-    //    if (opt_fullscreen)
-    //        ImGui::PopStyleVar(2);
+        if (opt_fullscreen)
+            ImGui::PopStyleVar(2);
 
-    //    // Submit the DockSpace
-    //    ImGuiIO& io = ImGui::GetIO();
-    //    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
-    //    {
-    //        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-    //        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-    //    }
+        // Submit the DockSpace
+        ImGuiIO& io = ImGui::GetIO();
+        if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+        {
+            ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+            ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+        }
 
-    //    if (ImGui::BeginMenuBar())
-    //    {
-    //        if (ImGui::BeginMenu("File"))
-    //        {
-    //            // Disabling fullscreen would allow the window to be moved to the front of other windows,
-    //            // which we can't undo at the moment without finer window depth/z control.
+        if (ImGui::BeginMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                // Disabling fullscreen would allow the window to be moved to the front of other windows,
+                // which we can't undo at the moment without finer window depth/z control.
 
-    //            if (ImGui::MenuItem("Exit"))TomCat::Application::Get().Close();
-    //            ImGui::EndMenu();
-    //        }
-    //        ImGui::EndMenuBar();
-    //    }
+                if (ImGui::MenuItem("Exit"))TomCat::Application::Get().Close();
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenuBar();
+        }
 
-    //    ImGui::Begin("Settings");
-    //    ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
+        ImGui::Begin("Settings");
+        ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 
-    //    uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
-    //    ImGui::Image((void*)textureID, ImVec2{ 1280, 720 });
+        uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
+        ImGui::Image((void*)textureID, ImVec2{ 1280, 720 });
 
-    //    ImGui::End();
+        ImGui::End();
 
-    //    ImGui::End();
-    //}
-    //else
-    //{
+        ImGui::End();
+    }
+    else
+    {
+        ImGui::Begin("Settings");
+        ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 
-    //}
+        uint32_t textureID = m_CheckerboardTexture->GetRendererID();
+        ImGui::Image((void*)textureID, ImVec2{ 1280, 720 });
 
-    ImGui::Begin("Settings");
-    ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
-
-    //uint32_t textureID = m_CheckerboardTexture->GetRendererID();
-    //ImGui::Image((void*)textureID, ImVec2{ 1280, 720 });
-
-    ImGui::End();
+        ImGui::End();
+    }
 
 }
 
