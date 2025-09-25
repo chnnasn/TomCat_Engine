643a1f3bbc2a6f7a2b47d80081c1d231be2ebc96
ch <127966448+chnnasn@users.noreply.github.com>
2025-09-25 14:47:12 +0800

    Day 41: Profiling tool

--- Z_Examples/src/Z_Examples2D.cpp
+++ Z_Examples/src/Z_Examples2D.cpp
@@ -3,6 +3,45 @@
 
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <chrono>
+
+
+template<typename Fn>
+class Timer
+{
+public:
+    Timer(const char* name, Fn&& func)
+        : m_Name(name), m_Func(func), m_Stopped(false)
+    {
+        m_StartTimepoint = std::chrono::high_resolution_clock::now();
+    }
+
+    ~Timer()
+    {
+        if (!m_Stopped)
+            Stop();
+    }
+
+    void Stop()
+    {
+        auto endTimepoint = std::chrono::high_resolution_clock::now();
+
+        long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+        long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+
+        m_Stopped = true;
+
+        float duration = (end - start) * 0.001f;
+        m_Func({ m_Name, duration });
+    }
+private:
+    const char* m_Name;
+    Fn m_Func;
+    std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
+    bool m_Stopped;
+};
+
+#define PROFILE_SCOPE(name) Timer timer##__LINE__(name, [&](ProfileResult profileResult) { m_ProfileResults.push_back(profileResult); })
 
 
 Z_Examples2D::Z_Examples2D():Layer("Z_Examples2D"),m_CameraController(1280.0f / 720.0f)
@@ -22,18 +61,30 @@ void Z_Examples2D::OnDetach()
 
 void Z_Examples2D::OnUpdate(TomCat::Timestep ts)
 {
+    PROFILE_SCOPE("Sandbox2D::OnUpdate");
+
 	//调用摄像机
-	m_CameraController.OnUpdate(ts);
+    m_CameraController.OnUpdate(ts);
+    {
+        PROFILE_SCOPE("CameraController::OnUpdate");
+        m_CameraController.OnUpdate(ts);
+    }
 
 	//渲染
-	TomCat::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
-	TomCat::RenderCommand::Clear();
+    {
+        PROFILE_SCOPE("Renderer Prep");
+        TomCat::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+        TomCat::RenderCommand::Clear();
+    }
 
-	TomCat::Renderer2D::BeginScene(m_CameraController.GetCamera());
-    TomCat::Renderer2D::DrawQuad({-1.0f,0.0f}, {0.8f,0.8f}, {1.0f,1.0f,1.0f,1.0f});
-    TomCat::Renderer2D::DrawQuad({0.5f,-0.5f}, {0.5f,0.75f},{0.2f,0.3f,0.8f,1.0f});
-    TomCat::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
-    TomCat::Renderer2D::EndScene();
+    {
+        PROFILE_SCOPE("Renderer Draw");
+        TomCat::Renderer2D::BeginScene(m_CameraController.GetCamera());
+        TomCat::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+        TomCat::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
+        TomCat::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
+        TomCat::Renderer2D::EndScene();
+    }
 
 
 }
@@ -108,6 +159,16 @@ void Z_Examples2D::OnImGuiRender()
 
     ImGui::Begin("Settings");
     ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
+
+    for (auto& result : m_ProfileResults)
+    {
+        char label[50];
+        strcpy(label, "%.3fms ");
+        strcat(label, result.Name);
+        ImGui::Text(label, result.Time);
+    }
+    m_ProfileResults.clear();
+
     //uint32_t textureID = m_checkerboardTexture->GetRendererID();
     //ImGui::Image((void*)textureID->GetRendererID(), ImVec2{64.0f,64.0f});
 
