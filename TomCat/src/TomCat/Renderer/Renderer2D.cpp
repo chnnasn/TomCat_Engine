ca4ed933fef6d6a3c3aea039ea72e664c9dcb27b
ch <127966448+chnnasn@users.noreply.github.com>
2025-09-25 13:33:22 +0800

    Day39:新增渲染器纹理

--- TomCat/src/TomCat/Renderer/Renderer2D.cpp
+++ TomCat/src/TomCat/Renderer/Renderer2D.cpp
@@ -2,15 +2,16 @@
 #include"Renderer2D.h"
 #include"VertexArray.h"
 #include"shader.h"
-#include "platform/OpenGL/OpenGLShader.h"
 #include "RenderCommand.h"
-namespace	TomCat {
+#include <glm/gtc/matrix_transform.hpp>
 
+namespace	TomCat {
 
 	struct  Renderer2DStorage
 	{
 		Ref<VertexArray> QuadVertexArray;
 		Ref<Shader> FlatColorShader;
+		Ref<Shader> TextureShader;
 	};
 
 	static Renderer2DStorage* m_data;
@@ -22,10 +23,10 @@ namespace	TomCat {
 		m_data->QuadVertexArray = VertexArray::Create();
 
 		float squareVertices[5 * 4] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.5f,  0.5f, 0.0f,
-			-0.5f,  0.5f, 0.0f
+			-0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+			 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+			 0.5f,  0.5f, 0.0f, 1.0f, 1.0f,
+			-0.5f,  0.5f, 0.0f, 0.0f, 1.0f
 		};
 
 
@@ -33,7 +34,8 @@ namespace	TomCat {
 		squareVB.reset(VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
 
 		BufferLayout squareVBLayout = {
-			{ShaderDataType::Float3,"a_Posioton"},
+			{ ShaderDataType::Float3, "a_Position" },
+			{ ShaderDataType::Float2, "a_TexCoord" }
 		};
 		squareVB->SetLayout(squareVBLayout);
 		m_data->QuadVertexArray->AddVertexBuffer(squareVB);
@@ -44,7 +46,9 @@ namespace	TomCat {
 		m_data->QuadVertexArray->SetIndexBuffer(squareIB);
 
 		m_data->FlatColorShader = Shader::Create("assets/shaders/FlatColor.glsl");
-
+		m_data->TextureShader = Shader::Create("assets/shaders/Texture.glsl");
+		m_data->TextureShader->Bind();
+		m_data->TextureShader->SetInt("u_Texture",0);
 
 	}
 	void Renderer2D::Shutdown()
@@ -53,9 +57,12 @@ namespace	TomCat {
 	}
 	void Renderer2D::BeginScene(const OrthographicCamera& carmera)
 	{
-		std::dynamic_pointer_cast<TomCat::OpenGLShader>(m_data->FlatColorShader)->Bind();
-		std::dynamic_pointer_cast<TomCat::OpenGLShader>(m_data->FlatColorShader)->UploadUniformMat4("u_ViewProjection",carmera.GetViewProjectionMatrix());
-		std::dynamic_pointer_cast<TomCat::OpenGLShader>(m_data->FlatColorShader)->UploadUniformMat4("u_Transform",glm::mat4(1.0f));
+		m_data->FlatColorShader->Bind();
+		m_data->FlatColorShader->SetMat4("u_ViewProjection",carmera.GetViewProjectionMatrix());
+
+		m_data->TextureShader->Bind();
+		m_data->TextureShader->SetMat4("u_ViewProjection",carmera.GetViewProjectionMatrix());
+
 	}
 	void Renderer2D::EndScene()
 	{
@@ -67,8 +74,31 @@ namespace	TomCat {
 	}
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
 	{
-		std::dynamic_pointer_cast<TomCat::OpenGLShader>(m_data->FlatColorShader)->Bind();
-		std::dynamic_pointer_cast<TomCat::OpenGLShader>(m_data->FlatColorShader)->UploadUniformFloat4("u_Color", color);
+		m_data->FlatColorShader->Bind();
+		m_data->FlatColorShader->SetFloat4("u_Color", color);
+
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+		m_data->FlatColorShader->SetMat4("u_Transform", transform	);
+
+		m_data->QuadVertexArray->Bind();
+		RenderCommand::DrawIndexed(m_data->QuadVertexArray);
+	}
+
+	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture)
+	{
+		DrawQuad({ position.x,position.y,0.0f }, size, texture);
+
+	}
+
+	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture)
+	{
+
+		m_data->TextureShader->Bind();
+
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+		m_data->TextureShader->SetMat4("u_Transform", transform);
+
+		texture->Bind();
 
 		m_data->QuadVertexArray->Bind();
 		RenderCommand::DrawIndexed(m_data->QuadVertexArray);
--- Z_Examples/src/Z_Examples2D.h
+++ Z_Examples/src/Z_Examples2D.h
@@ -23,5 +23,7 @@ private:
 
 	TomCat::Ref<TomCat::Shader> m_FlatColorShader;
 
+	TomCat::Ref<TomCat::Texture2D> m_CheckerboardTexture;
+
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f,1.0f };
 };
