643bc5f5a6d19ea29fa2774caa2c7215d07cf14c
ch <127966448+chnnasn@users.noreply.github.com>
2025-09-25 14:15:44 +0800

    Day 40: Merge color and texture shaders

--- TomCat/src/platform/OpenGL/OpenGLTexture.h
+++ TomCat/src/platform/OpenGL/OpenGLTexture.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "TomCat/Renderer/Texture.h"
+#include <glad/glad.h>
 
 namespace TomCat {
 
 	class OpenGLTexture2D : public Texture2D
 	{
 	public:
+		OpenGLTexture2D(uint32_t width, uint32_t height);
 		OpenGLTexture2D(const std::string& path);
 		virtual ~OpenGLTexture2D();
 
@@ -14,7 +16,7 @@ namespace TomCat {
 
 		virtual uint32_t GetWidth() const override { return m_Width; };
 		virtual uint32_t GetHeight() const override { return m_Height; };
-		virtual uint32_t GetRendererID()const override { return m_RendererID; };
+		virtual void SetData(void* data, uint32_t size) override;
 
 		virtual void Bind(uint32_t slot = 0) const override;
 
@@ -22,6 +24,7 @@ namespace TomCat {
 		std::string m_Path;
 		uint32_t m_Width,m_Height;
 		uint32_t m_RendererID;
+		GLenum m_InternalFormat, m_DataFormat;
 	};
 
 
