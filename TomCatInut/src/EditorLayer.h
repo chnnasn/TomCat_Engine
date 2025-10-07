fc874b2357ff93def0f0e7b4b858b344c77400b2
ch <127966448+chnnasn@users.noreply.github.com>
2025-10-07 23:10:20 +0800

    Day67:为鼠标准备帧缓冲区

--- TomCatInut/src/EditorLayer.h
+++ TomCatInut/src/EditorLayer.h
@@ -37,6 +37,8 @@ namespace TomCat {
 		Entity m_CameraEntity;
 		Entity m_SecondCamera;
 
+		Entity m_HoveredEntity;
+
 		bool m_PrimaryCamera = true;
 
 		EditorCamera m_EditorCamera;
@@ -45,6 +47,8 @@ namespace TomCat {
 
 		bool m_ViewportFocused = false, m_ViewportHovered = false;
 		glm::vec2 m_ViewportSize = { 0.0f, 0.0f };
+
+		glm::vec2 m_ViewportBounds[2];
 
 		glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 
