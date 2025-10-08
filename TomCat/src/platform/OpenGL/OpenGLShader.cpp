89c814f5b84230eb37202296d6899a0fd9e8a2d3
ch <127966448+chnnasn@users.noreply.github.com>
2025-10-08 11:46:33 +0800

    修护premake路径报错的问题

--- TomCat/src/platform/OpenGL/OpenGLShader.cpp
+++ TomCat/src/platform/OpenGL/OpenGLShader.cpp
@@ -225,6 +225,13 @@ namespace TomCat {
 		UploadUniformFloat(name, value);
 	}
 
+	void OpenGLShader::SetFloat2(const std::string& name, const glm::vec2& value)
+	{
+		TC_PROFILE_FUNCTION();
+
+		UploadUniformFloat2(name, value);
+	}
+
 	 void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	 {
 		 TC_PROFILE_FUNCTION();
